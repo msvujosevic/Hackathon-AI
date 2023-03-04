53eabfc3687bbad0c5bb8a28856e46471acc048c
Marko Vujosevic <msvujosevic@gmail.com>
2023-03-03 22:56:12 -0600

    show images

---
+++ Neural Network.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+mnist = tf.keras.datasets.mnist
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+plt.figure(figsize=(10,10))
+for i in range(25):
+    plt.subplot(5,5,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(train_images[i], cmap=plt.cm.binary)
+    plt.xlabel(train_labels[i])
+plt.show()
+
